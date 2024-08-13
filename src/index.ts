a1bfad496cb35154adcc54ca9895eade18047cd4
hasanify <hasanali.abps@gmail.com>
2024-08-13 20:20:12 +0530

    print version on sertup

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,12 @@ import "dotenv/config";
 import "module-alias/register";
 
 import handleMqtt from "@/handler";
+import { readFileSync } from "fs";
+import path from "path";
 
+const packageJsonPath = path.join(__dirname, "..", "package.json");
+const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
+console.log("--------------------------------------------");
+console.log(`Started v${packageJson.version}! 🚀`);
+console.log("--------------------------------------------");
 handleMqtt();
